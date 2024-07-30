cd1a795dfbc23de58be1351ab7ff6418a0f94469
nihalshameem <nihalshameem6@gmail.com>
2024-07-30 21:38:27 +0530

    todo and todo crud completed

--- models/todo.js
+++ models/todo.js
@@ -1,12 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const todoSchema = new Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-});
+const todoSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    assigned_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model for reference
+      required: true,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model for reference
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    updated_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model for reference
+      required: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Assuming you have a User model for reference
+      required: true,
+    },
+    task_date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
 module.exports = Todo;
--- routes/todo.js
+++ routes/todo.js
@@ -1,36 +1,51 @@
-const router = require('express').Router();
-let Todo = require('../models/todo');
+const router = require("express").Router();
+let Todo = require("../models/todo");
 
 // Get all todos
-router.route('/').get((req, res) => {
-  Todo.find()
-    .then(todos => res.json(todos))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+  Todo.find({ user_id: req.user.id })
+    .then((todos) => res.json(todos))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Add new todo
-router.route('/add').post((req, res) => {
-  const text = req.body.text;
+router.route("/add").post((req, res) => {
 
-  const newTodo = new Todo({ text });
+  const newTodo = new Todo({
+    text: req.body.text,
+    assigned_by: req.user.id,
+    user_id: req.body.id,
+    completed: false,
+    updated_by: req.user.id,
+    created_by: req.user.id,
+    task_date: req.body.task_date,
+  });
 
-  newTodo.save()
-    .then(() => res.json('Todo added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  newTodo
+    .save()
+    .then(() => res.json("Todo added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Update todo by ID
-router.route('/:id').put((req, res) => {
-  Todo.findByIdAndUpdate(req.params.id, { completed: req.body.completed })
-    .then(() => res.json('Todo updated!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/:id").put((req, res) => {
+  Todo.findByIdAndUpdate(req.params.id, {
+    text: req.body.text,
+    user_id: req.body.user_id,
+    completed: req.body.completed,
+    updated_by: req.user.id,
+    assigned_by: req.user.id,
+    task_date: req.body.task_date,
+  })
+    .then(() => res.json("Todo updated!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Delete todo by ID
-router.route('/:id').delete((req, res) => {
+router.route("/:id").delete((req, res) => {
   Todo.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Todo deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => res.json("Todo deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
